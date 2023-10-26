a63e77f0d27f83f000a9c063379213c79201e0ec
Trinethr N <trinethr_n@protonmail.com>
2023-10-26 23:07:55 +0530

    updated fancyclock to show date

--- src/components/FancyClock.vue
+++ src/components/FancyClock.vue
@@ -5,6 +5,7 @@ const day = ref("MONDAY");
 const hr = ref(0);
 const min = ref(0);
 const sec = ref(0);
+const datetext = ref("");
 
 const days = [
   "SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"
@@ -16,6 +17,7 @@ const refresh = () => {
   min.value = date.getMinutes();
   sec.value = date.getSeconds();
   day.value = days[date.getDay()];
+  datetext.value = String(date.getDate()).padStart(2, '0') + "/" + String(date.getMonth() + 1).padStart(2, '0') + "/" + String(date.getFullYear()).padStart(4, '0');
 }
 
 refresh();
@@ -27,7 +29,7 @@ setInterval(refresh, 1000);
 <template>
   <div class="flex flex-col gap-2 text-center select-none text-blue-950 text-opacity-80 pb-24">
     <div class="day pl-8 text-8xl">{{ day }}</div>
-    <div class="date text-3xl">{{ String(hr).padStart(2, '0') }}:{{ String(min).padStart(2, '0') }}:{{ String(sec).padStart(2, '0') }}</div>
+    <div class="datetime text-2xl">{{ datetext }} ⚬ {{ String(hr).padStart(2, '0') }}:{{ String(min).padStart(2, '0') }}:{{ String(sec).padStart(2, '0') }}</div>
   </div>
 </template>
 
@@ -36,7 +38,7 @@ setInterval(refresh, 1000);
   font-family: "Anurati Regular";
   letter-spacing: 2rem;
 }
-.date {
+.datetime {
   font-family: "Space Mono";
   letter-spacing: 0.25rem;
 }
